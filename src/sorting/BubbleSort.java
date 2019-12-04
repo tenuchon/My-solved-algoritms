a1377b6d3cf7ab7418a0637b0c95064479731476
Andrei Ten <tenuchon@gmail.com>
2019-12-04 09:44:07 +0900

    Add BubbleSort

---
+++ src/sorting/BubbleSort.java
@@ -0,0 +1,34 @@
+package sorting;
+
+public class BubbleSort {
+
+    private static void toSort(int[] array) {
+        boolean isSorted = false;
+        while (!isSorted) {
+            isSorted = true;
+            for (int i = 1; i < array.length; i++) {
+                if (array[i] < array[i - 1]) {
+                    int temp = array[i];
+                    array[i] = array[i - 1];
+                    array[i - 1] = temp;
+                    isSorted = false;
+                }
+            }
+        }
+    }
+
+    private static void printArray(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int[] array = new int[]{18, 33, 28, 44, 12, 34, 57, 87};
+
+        printArray(array);
+        toSort(array);
+        printArray(array);
+    }
+}
